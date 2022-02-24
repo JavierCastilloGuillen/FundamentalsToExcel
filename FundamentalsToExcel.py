270239e24813a4e776e94bb401c780845957f7b4
Javier Castillo Guillén <84958748+JavierCastilloGuillen@users.noreply.github.com>
2022-02-24 05:54:59 +0000

    Removed [ ] from symbol on list in Dataframe
    Avoid error on second run on spot on the code on 'Price' tab on the excel

--- FundamentalsToExcel.py
+++ FundamentalsToExcel.py
@@ -14,35 +14,33 @@ sf.set_data_dir('simfin_data/')
 file = 'DCF.xlsx'  
 
 
-stock = pd.read_excel(file, sheet_name='Data')
+stock = pd.read_excel(file, sheet_name='Price')
+print(stock)
 symbol= stock['Symbol'][0]
 price = pdr.get_data_yahoo(symbol)['Adj Close'][-1]
-data = {'Symbol':[symbol],'Price':[price]}
-priceData = pd.DataFrame.from_dict(data)
-
-
-income = sf.load(dataset='income', variant='annual', market='us',
-              index=[TICKER, REPORT_DATE],
-              parse_dates=[REPORT_DATE, PUBLISH_DATE, RESTATED_DATE])
-income = income.loc[symbol].T
-income = income.reset_index().fillna(0)
-
-cashflow = sf.load(dataset='cashflow', variant='annual', market='us',
-              index=[TICKER, REPORT_DATE],
-              parse_dates=[REPORT_DATE, PUBLISH_DATE, RESTATED_DATE])
-cashflow = cashflow.loc[symbol].T
-cashflow = cashflow.reset_index().fillna(0)
-
-balance = sf.load(dataset='balance', variant='annual', market='us',
-              index=[TICKER, REPORT_DATE],
-              parse_dates=[REPORT_DATE, PUBLISH_DATE, RESTATED_DATE])
-balance = balance.loc[symbol].T
-balance = balance.reset_index().fillna(0)
-
+data = {'Symbol': symbol,'Price':[price]}
+priceData = pd.DataFrame(data=data)
 
 wb = pd.ExcelFile(file)
-
 with pd.ExcelWriter(wb, engine="openpyxl", mode='a', if_sheet_exists='replace') as writer:     
+
+    income = sf.load(dataset='income', variant='annual', market='us',
+                  index=[TICKER, REPORT_DATE],
+                  parse_dates=[REPORT_DATE, PUBLISH_DATE, RESTATED_DATE])
+    income = income.loc[symbol].T
+    income = income.reset_index().fillna(0)
+
+    cashflow = sf.load(dataset='cashflow', variant='annual', market='us',
+                  index=[TICKER, REPORT_DATE],
+                  parse_dates=[REPORT_DATE, PUBLISH_DATE, RESTATED_DATE])
+    cashflow = cashflow.loc[symbol].T
+    cashflow = cashflow.reset_index().fillna(0)
+
+    balance = sf.load(dataset='balance', variant='annual', market='us',
+                  index=[TICKER, REPORT_DATE],
+                  parse_dates=[REPORT_DATE, PUBLISH_DATE, RESTATED_DATE])
+    balance = balance.loc[symbol].T
+    balance = balance.reset_index().fillna(0)
     
     workBook = writer.book
     balance.to_excel(writer, sheet_name='Balance',index=False)
